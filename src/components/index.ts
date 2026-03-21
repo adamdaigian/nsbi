15797b27e39d74936d6d4ae88dda97a43c94e9ac
Adam Daigian <adam.daigian@gmail.com>
2026-03-21 12:57:09 -0700

    feat: update component exports for Vega-Lite architecture

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,32 +1,26 @@
 // Charts
-export { LineChart } from "./charts/LineChart";
-export { AreaChart } from "./charts/AreaChart";
-export { BarChart } from "./charts/BarChart";
-export { ScatterPlot } from "./charts/ScatterPlot";
-export { DataTable } from "./charts/DataTable";
-export { BigValue } from "./charts/BigValue";
-export { Delta } from "./charts/Delta";
-export { Sparkline } from "./charts/Sparkline";
-export { EChartsRaw } from "./charts/EChartsRaw";
+export { VegaChart } from './charts/VegaChart'
+export type { VegaChartProps } from './charts/VegaChart'
+export { northstarTheme, CHART_COLORS } from './charts/vega-theme'
+
+// Non-chart components (retained as-is)
+export { BigValue } from './charts/BigValue'
+export { Delta } from './charts/Delta'
+export { DataTable } from './charts/DataTable'
 
 // Layout
-export { Grid } from "./layout/Grid";
-export { Group } from "./layout/Group";
-export { Divider } from "./layout/Divider";
-export { Tabs, TabsList, TabsTrigger, TabsContent } from "./layout/Tabs";
+export { Grid } from './layout/Grid'
+export { Group } from './layout/Group'
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './layout/Tabs'
+export { Divider } from './layout/Divider'
 
 // Inputs
-export { FilterProvider, useFilterValue } from "./inputs/FilterContext";
-export { Dropdown } from "./inputs/Dropdown";
-export { DateRange } from "./inputs/DateRange";
-export { ButtonGroup } from "./inputs/ButtonGroup";
-export { TextInput } from "./inputs/TextInput";
-export { MultiSelect } from "./inputs/MultiSelect";
-export { Slider } from "./inputs/Slider";
-export { CheckboxFilter } from "./inputs/CheckboxFilter";
-export { DateInput } from "./inputs/DateInput";
-
-// Data
-export { QueryProvider, useQueryData } from "./QueryContext";
-export { withQueryData } from "./withQueryData";
-export { vizRegistry } from "./registry";
+export { FilterProvider, useFilterValue } from './inputs/FilterContext'
+export { Dropdown } from './inputs/Dropdown'
+export { MultiSelect } from './inputs/MultiSelect'
+export { ButtonGroup } from './inputs/ButtonGroup'
+export { TextInput } from './inputs/TextInput'
+export { Slider } from './inputs/Slider'
+export { DateInput } from './inputs/DateInput'
+export { DateRange } from './inputs/DateRange'
+export { CheckboxFilter } from './inputs/CheckboxFilter'
